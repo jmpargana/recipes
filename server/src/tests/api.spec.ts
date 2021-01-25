53b041972b46324a4591496dfb2de64e3bd015b6
jmpargana <icmjmp@protonmail.ch>
2021-01-25 23:28:50 +0000

    first supertest test set

--- server/src/tests/api.spec.ts
+++ server/src/tests/api.spec.ts
@@ -1,12 +1,21 @@
+import express from 'express'
 import request from 'supertest'
 import router from '../router'
+
+const app = express()
+app.use('/', router)
 
 describe('recipe\'s server api', () => {
   it('sanity test', () => {
   })
 
   it('GET recipe by id', async () => {
-    const result = await request(router).get('/recipe')
+    const result = await request(app).get('/recipe')
     expect(result.status).toEqual(200)
+  })
+
+  it('GET to other endpoints sould return 404', async () => {
+    const res = await request(app).get('/something')
+    expect(res.status).toEqual(404)
   })
 })
