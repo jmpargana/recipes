6bf91ed9279e7eff1d668aa450141e1957353a01
jmpargana <icmjmp@protonmail.ch>
2021-03-15 08:44:23 +0000

    first tests for Tags component

--- client/src/components/Tags.vue
+++ client/src/components/Tags.vue
@@ -1,28 +1,43 @@
 <template>
   <div class="flex flex-wrap">
-    <div 
+    <div
       v-for="(tag, i) in tags"
       :key="i"
       class="flex items-center mx-1 px-3 py-1 rounded border border-red-600 rounded-3xl text-gray-200 shadow-sm bg-red-600 mb-1"
     >
       <div>{{ tag }}</div>
-      <div class="ml-1 w-3 h-3" @click="store.commit('deleteTag', { index: i })">
-        <svg class="fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path 
+      <div
+        class="ml-1 w-3 h-3"
+        @click="store.commit('deleteTag', { index: i })"
+      >
+        <svg
+          class="fill-current"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
             d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
           />
-	</svg>
+        </svg>
       </div>
     </div>
   </div>
 </template>
 
-<script setup>
-import { computed } from 'vue'
-import { useStore } from 'vuex'
+<script>
+import { computed } from "vue";
+import { useStore } from "vuex";
 
-const store = useStore()
-// FIXME: Add colors
+export default {
+  setup() {
+    const store = useStore();
+    // FIXME: Add colors
 
-const tags = computed(() => store.state.tags)
+    const tags = computed(() => store.state.tags);
+
+    return {
+      tags,
+    };
+  },
+};
 </script>
