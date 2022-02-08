de240a76e4e131813732c8574531149a1dc5cdc7
Joao Pargana <joao.s.silva@linkconsulting.com>
2022-02-08 19:51:38 +0000

    default css styles

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -3,10 +3,19 @@
 	import RecipeList from '../lib/recipe-list.svelte';
 </script>
 
+
+<h1>Share Your Recipes</h1>
+<h2>Share Your Recipes</h2>
+<h3>Share Your Recipes</h3>
+<h4>Share Your Recipes</h4>
+<h5>Share Your Recipes</h5>
+<h6>Share Your Recipes</h6>
+<p>Share Your Recipes</p>
+
 <a href="/new-recipe">New Recipe</a>
 
 <div class="homepage-container">
-	<SearchBlock />
+	<!-- <SearchBlock /> -->
 	<RecipeList />
 </div>
 
