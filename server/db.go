fad1c95b76dc47a6f01d6e12fe8105dbb636d2ac
jmpargana <joao.s.silva@linkconsulting.com>
2022-02-15 23:09:25 +0000

    update env vars

--- server/db.go
+++ server/db.go
@@ -8,19 +8,42 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+func mustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		log.Fatalf("Warning: %s environment variable not set.\n", k)
+	}
+	return v
+}
+
+var (
+        dbUser                 = mustGetenv("DB_USER")                  // e.g. 'my-db-user'
+        dbPwd                  = mustGetenv("DB_PASS")                  // e.g. 'my-db-password'
+        instanceConnectionName = mustGetenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
+        dbName                 = mustGetenv("DB_NAME")                  // e.g. 'my-database'
+)
+
+
+
 func SetupDB() *sqlx.DB {
+  socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
+  if !isSet {
+          socketDir = "/cloudsql"
+  }
+
+  dbURI := fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true", dbUser, dbPwd, socketDir, instanceConnectionName, dbName)
 
 
 	// dsn := fmt.Sprintf("host=")
-	connStr := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
-		os.Getenv("PQ_USER"),
-		os.Getenv("PQ_PASS"),
-		os.Getenv("PQ_HOST"),
-    "5432",
-		os.Getenv("PQ_DB"),
-)
+	// connStr := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
+	// 	os.Getenv("PQ_USER"),
+	// 	os.Getenv("PQ_PASS"),
+	// 	os.Getenv("PQ_HOST"),
+    // "5432",
+	// 	os.Getenv("PQ_DB"),
+// )
 
-	db, err := sqlx.Connect("postgres",connStr)
+	db, err := sqlx.Connect("postgres",dbURI)
 	if err != nil {
 		log.Fatalln(err)
 	}
